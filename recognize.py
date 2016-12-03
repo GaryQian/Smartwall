adfc794845b733fa21a59e514dc22e25a78dccfb
Sarah Sukardi <sukardi@jhu.edu>
2016-12-02 19:01:37 -0500

    well.. the output matches the one for the cifar10 sample.

--- recognize.py
+++ recognize.py
@@ -50,9 +50,9 @@ num_classes = 1
 
 # Create the model
 model = Sequential()
-model.add(Convolution2D(32, 32, 3, input_shape=(32, 32, 3), border_mode='same', activation='relu', W_constraint=maxnorm(3)))
+model.add(Convolution2D(32, 3, 3, input_shape=(3, 32, 32), border_mode='same', activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Convolution2D(32, 32, 3, activation='relu', border_mode='same', W_constraint=maxnorm(3)))
+model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same', W_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
