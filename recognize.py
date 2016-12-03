04527f6eb9b019856d5c9bbb9e0aab1dfbf7e41d
Sarah Sukardi <sukardi@jhu.edu>
2016-12-02 19:27:28 -0500

    Debugging

--- recognize.py
+++ recognize.py
@@ -13,6 +13,13 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+
+
+# (X_train1, y_train1), (X_test1, y_test1) = cifar10.load_data()
+# print(X_train1.shape)
+# print(y_train1.shape)
+
+
 K.set_image_dim_ordering('th')
 
 # fix random seed for reproducibility
@@ -32,21 +39,39 @@ imNum0 = 10000
 imNum1 = 10000
 
 shape0 = (imNum0, 32, 32, 3)
-shape1 = (imNum1, 32, 32, 3)
-openHand = np.zeros(shape0)     #np.array for open hand images
-closedHand = np.zeros(shape1)   #np.array for closed hand images
 
+num_classes = 2
+X_train = np.ndarray(shape0)
+y_train = np.ndarray((imNum0))
+
+count = 0
 for x in range(0, imNum0): #for each image in trainingData/0/*
   fname = prestring0 + str(x) + postString
   if path.exists(fname):
     temp = cv2.imread(fname)
-    openHand[x] = temp
+    X_train[count] = temp
+    y_train[count] = 0
+
+    count += 1
     # for rgb in range (0, 3):
     # openHand[x][rgb] = temp[:,:][rgb] #load it into openHand
-  else:
-    imNum0 += 1
-  
-num_classes = 1
+
+print(X_train.shape)
+
+for x in range(0, imNum1): #for each image in trainingData/0/*
+  fname = prestring1 + str(x) + postString
+  if path.exists(fname):
+    temp = cv2.imread(fname)
+    X_train[count] = temp
+    y_train[count] = 1
+
+    count += 1
+    # for rgb in range (0, 3):
+    # openHand[x][rgb] = temp[:,:][rgb] #load it into openHand
+
+X_train = X_train[:count]
+
+
 
 # Create the model
 model = Sequential()
@@ -66,10 +91,12 @@ sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 print(model.summary())
 
+print(X_train.shape)
 
-"""
+
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=epochs, batch_size=32)
+model.fit(X_train, y_train, validation_data=(X_train, y_train), nb_epoch=epochs, batch_size=32)
+"""
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
