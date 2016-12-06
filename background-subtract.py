68d5d7c35b8a1baabd25365e0f0bc270b61c61bc
Tony Jiang <tjiang11@jhu.edu>
2016-12-05 23:30:18 -0500

    Improved background subtract.

--- background-subtract.py
+++ background-subtract.py
@@ -8,16 +8,15 @@ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 min_YCrCb = np.array([0,133,77],np.uint8)
 max_YCrCb = np.array([255,173,127],np.uint8)
 
-##how to change parameters to increase threshold???
-##MOG1 seems like it would work really well if we could actually tweak the thresholding.
-subtractor = cv2.BackgroundSubtractorMOG(history=100000000, nmixtures=5, backgroundRatio=0.1, noiseSigma=0)
+min_RGB = np.array([130, 100, 0], np.uint8)
+max_RGB = np.array([255, 223, 196], np.uint8)
 
-#subtractor = cv2.BackgroundSubtractorMOG2(history=100, varThreshold=20.0, bShadowDetection=True)
+subtractor = cv2.BackgroundSubtractorMOG2(history=100, varThreshold=5.0)
 
 while(True):
     ret, frame = cap.read()
 
-    fgmask = subtractor.apply(frame, learningRate=0)
+    fgmask = subtractor.apply(frame, learningRate=0.005)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
     fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR);
@@ -25,20 +24,24 @@ while(True):
     fgmask = (fgmask / 255) * frame
 
     # Convert image to YCrCb
-    imageYCrCb = cv2.cvtColor(fgmask,cv2.COLOR_BGR2YCR_CB)
+    image = cv2.cvtColor(fgmask,cv2.COLOR_BGR2YCR_CB)
 
     # Find region with skin tone in YCrCb image
-    skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-
+    # skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+    skinRegion2 = cv2.inRange(image, min_RGB, max_RGB)
     # Do contour detection on skin region
-    contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(skinRegion2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # Draw the contour on the source image
 
     count = 0
 
     for i, c in enumerate(contours):
         area = cv2.contourArea(c)
-        if area > 200 and area < 1000:
+        x,y,w,h = cv2.boundingRect(c)
+        w_over_h = w / h
+        h_over_w = h / w
+        extent = area / (w * h)
+        if area > 100 and area < 1500 and w_over_h < 4 and h_over_w < 4 and extent > 0.25:
             count += 1
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
